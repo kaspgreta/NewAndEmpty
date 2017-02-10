62edce197eade606690084de94219db68dadc23e
Your Name <greta.kasparaityte@gmail.com>
2017-02-10 16:00:03 +0200

    Project init

---
+++ src/lt/swedbank/itacademy/New.java
@@ -0,0 +1,10 @@
+package lt.swedbank.itacademy;
+
+/**
+ * Created by Greta on 2017-02-10.
+ */
+public class New {
+    public static void main(String[] args){
+        System.out.println("Kodel");
+    }
+}
